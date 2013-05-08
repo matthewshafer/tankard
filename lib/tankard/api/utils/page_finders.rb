79f5c20efdc1d9bae537903299f0fe203c316fbb
Matthew Shafer <matthewshafer@mac.com>
2013-05-08 00:00:52 -0700

    Remove options clone from find_on_all_pages

--- lib/tankard/api/utils/page_finders.rb
+++ lib/tankard/api/utils/page_finders.rb
@@ -9,12 +9,7 @@ module Tankard
         private
           def find_on_all_pages(uri, request, options, block)
             page = 0
-            # the clone is here just in case someone decides to create an api class of their own
-            # and not go through client.  If they re-use the api class they have created then
-            # we would possibably have additional options set that they would reuse with subsequent calls
-            # honestly though no one should ever create an api class of their own to use, its a bad idea
-            # might pull out this clone at some point
-            options = options.clone
+            
             begin
               page += 1
               options[:p] = page
