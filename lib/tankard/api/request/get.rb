e1786156618b7731912f97fc61616a61e3d1ec87
Matthew Shafer <matthewshafer@mac.com>
2014-03-17 16:29:03 -0700

    remove redundant begin

--- lib/tankard/api/request/get.rb
+++ lib/tankard/api/request/get.rb
@@ -6,11 +6,9 @@ module Tankard
         private
 
           def request_data_with_nil_on_http_error(request_object, uri, options)
-            begin
-              request_data(request_object, uri, options)
-            rescue Tankard::Error::HttpError
-              nil
-            end
+            request_data(request_object, uri, options)
+          rescue Tankard::Error::HttpError
+            nil
           end
 
           # break up the request methods into smaller pieces
