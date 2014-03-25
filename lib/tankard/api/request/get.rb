5a59935bc8d50c56b13f2a69df6f350631e26462
Matthew Shafer <matthewshafer@mac.com>
2014-03-24 17:45:03 -0700

    remove request_data method from Tankard::Api::Request::Get because it was not really used anywhere.  Moved the data from that method to request_data_with_nil_on_http_error

--- lib/tankard/api/request/get.rb
+++ lib/tankard/api/request/get.rb
@@ -9,14 +9,9 @@ module Tankard
       private
 
         def request_data_with_nil_on_http_error(request_object, uri, options)
-          request_data(request_object, uri, options)
+          get_request(request_object, uri, options)['data']
         rescue Tankard::Error::HttpError
           nil
-        end
-
-        # break up the request methods into smaller pieces
-        def request_data(request_object, uri, options)
-          get_request(request_object, uri, options)['data']
         end
 
         def get_request(request_object, uri, options)
