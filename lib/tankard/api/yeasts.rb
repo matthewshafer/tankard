c0eac9a412159de488e1a45590fdd652076e6aee
Matthew Shafer <matthewshafer@mac.com>
2014-04-17 22:47:56 -0700

    fixing some code documentation in yeasts.rb

--- lib/tankard/api/yeasts.rb
+++ lib/tankard/api/yeasts.rb
@@ -23,9 +23,9 @@ module Tankard
       end
 
       # @!method each(&block)
-      #   Calls the given block once for each style
+      #   Calls the given block once for each yeast
       #
-      #   @yieldparam [Hash] hash containing individual style information
+      #   @yieldparam [Hash] hash containing individual yeast's information
       #   @raise [Tankard::Error::ApiKeyUnauthorized] when an api key is not valid
       #   @raise [Tankard::Error::InvalidResponse] when no data is returned fron the api
       #   @raise [Tankard::Error::HttpError] when a status other than 200 or 401 is returned
