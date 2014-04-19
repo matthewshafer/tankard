bde991846c6b80e7d05f4c2a510fc430cbe2d1e2
Matthew Shafer <matthewshafer@mac.com>
2014-04-18 23:09:52 -0700

    remove page_finder from style.  Now it only has the find method (which should work better).  I also refactored a few things in style.

--- lib/tankard/api/style.rb
+++ lib/tankard/api/style.rb
@@ -1,5 +1,3 @@
-require 'hashie'
-require 'tankard/api/utils/page_finders'
 require 'tankard/api/utils/find'
 
 module Tankard
@@ -9,9 +7,7 @@ module Tankard
     # @see http://www.brewerydb.com/developers/docs-endpoint/style_index
     # @author Matthew Shafer
     class Style
-      include Tankard::Api::Utils::PageFinders
       include Tankard::Api::Utils::Find
-      # @!parse include ::Enumerable
 
       # Initializes a new object
       #
@@ -20,7 +16,8 @@ module Tankard
       # @return [Tankard::Api::Style]
       def initialize(request, options = {})
         @http_client = request
-        @http_request_parameters = Hashie::Mash.new(options)
+        @http_request_parameters = options
+        @route = 'style'
       end
 
       # @!method find(id_or_array, options={})
@@ -32,43 +29,11 @@ module Tankard
       #     if an array is passed to find an array containing hashes with each style is returned.
       #     if a style is not found nothing for that style is returned.
 
-      # @!method each(&block)
-      #   Calls the given block once for each style
-      #
-      #   @yieldparam [Hash] hash containing individual style information
-      #   @raise [Tankard::Error::MissingParameter] when the id is not set
-      #   @raise [Tankard::Error::ApiKeyUnauthorized] when an api key is not valid
-      #   @raise [Tankard::Error::InvalidResponse] when no data is returned fron the api
-      #   @raise [Tankard::Error::HttpError] when a status other than 200 or 401 is returned
-      #   @raise [Tankard::Error::LoadError] when multi json is unable to decode json
-
-      # Style id to query
-      #
-      # @param style_id [String]
-      # @return [self] returns itself
-      def id(style_id)
-        @http_request_parameters.id = style_id
-        self
-      end
-
     private
 
       attr_reader :http_client
       attr_reader :http_request_parameters
-
-      def raise_if_no_id_in_options
-        @style_id = @http_request_parameters.delete(:id) if @http_request_parameters.id?
-        fail Tankard::Error::MissingParameter, 'No style id set' unless @style_id
-        @style_id
-      end
-
-      def route
-        'style'
-      end
-
-      def http_request_uri
-        "#{route}/#{raise_if_no_id_in_options}"
-      end
+      attr_reader :route
     end
   end
 end
--- spec/tankard/api/style_spec.rb
+++ spec/tankard/api/style_spec.rb
@@ -25,103 +25,12 @@ describe Tankard::Api::Style do
     end
   end
 
-  describe '#id' do
-
-    it 'sets the options[:id] for the style id passed in' do
-      style.id(1)
-      style_options = style.instance_variable_get(:"@http_request_parameters")
-      expect(style_options[:id]).to eql(1)
-    end
-
-    it 'returns itself' do
-      expect(style.object_id).to eql(style.id(1).object_id)
-    end
-  end
-
-  describe 'when making a request' do
-
-    context 'and the id for a style is not set' do
-
-      it 'raises a Tankard::Error::NoStyleId' do
-        expect { style.each { |s| p s } }.to raise_error(Tankard::Error::MissingParameter, 'No style id set')
-      end
-    end
-
-    context 'and the id for a style is set' do
-
-      before do
-        @request.stub(:get).with('style/1', {}).and_return('data' => ['style_valid'])
-      end
-
-      it 'uses the style id in the uri' do
-        expect(style.id(1).map { |x| x }).to eql(['style_valid'])
-      end
-    end
-  end
-
   describe 'private methods' do
-
-    describe '#raise_if_no_id_in_options' do
-
-      context 'when an ID is not set' do
-
-        it 'raises Tankard::Error::MissingParameter' do
-          expect { style.send(:raise_if_no_id_in_options) }.to raise_error(Tankard::Error::MissingParameter, 'No style id set')
-        end
-      end
-
-      context 'when an ID is set' do
-
-        before do
-          style.instance_variable_get(:"@http_request_parameters")[:id] = 'test'
-        end
-
-        it 'returns the id from options' do
-          expect(style.send(:raise_if_no_id_in_options)).to eql('test')
-        end
-
-        it 'removes the id from options' do
-          style.send(:raise_if_no_id_in_options)
-          expect(style.instance_variable_get(:"@http_request_parameters")[:id]).to be_nil
-        end
-
-        it 'can be called multiple times and not raise error' do
-          style.send(:raise_if_no_id_in_options)
-          expect { style.send(:raise_if_no_id_in_options) }.not_to raise_error
-        end
-
-        it 'caches the ID for future method calls' do
-          style.send(:raise_if_no_id_in_options)
-          expect(style.send(:raise_if_no_id_in_options)).to eql('test')
-        end
-
-        it 'updates the cache value if the user sets a new ID' do
-          style.send(:raise_if_no_id_in_options)
-          style.instance_variable_get(:"@http_request_parameters")[:id] = 'test1'
-          expect(style.send(:raise_if_no_id_in_options)).to eql('test1')
-        end
-      end
-    end
 
     describe '#route' do
 
       it 'returns the route for the api request' do
         expect(style.send(:route)).to eql('style')
-      end
-    end
-
-    describe '#http_request_uri' do
-
-      before do
-        style.stub(:route).and_return('style')
-        style.stub(:raise_if_no_id_in_options).and_return('123')
-      end
-
-      context 'no endpoint is set' do
-
-        it 'returns the route with the id' do
-          expect(style.send(:http_request_uri)).to eql('style/123')
-        end
       end
     end
 
