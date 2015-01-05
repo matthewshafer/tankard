9b340b785feb9d75d6a6502a2c9cfeed8086897c
Matthew Shafer <matthewshafer@mac.com>
2015-01-05 00:24:32 -0800

    adding documentation to Tankard::Api::Adjunct. [ci skip]

--- lib/tankard/api/adjunct.rb
+++ lib/tankard/api/adjunct.rb
@@ -8,6 +8,22 @@ module Tankard
     # @see http://www.brewerydb.com/developers/docs-endpoint/adjunct_index
     # @author Matthew Shafer
     class Adjunct < Tankard::Api::Base::Find
+      # @!method initialize(request, options = {})
+      #   Initializes a new object
+      #
+      #   @param request [Tankard::Request]
+      #   @param options [Hash]
+      #   @return [Tankard::Api::Adjunct]
+
+      # @!method find(id_or_array, options={})
+      #   Find a single or multiple adjunct's by their id
+      #
+      #   @param id_or_array [String, Array]
+      #   @param options [Hash]
+      #   @return [Hash, Array] if a string with a adjunct id is passed to find then the hash of the adjunct's data is returned.
+      #     if an array is passed to find an array containing hashes with each adjunct's data is returned.
+      #     if an adjunct is not found nothing for that adjunct is returned.
+
     private
 
       def route
